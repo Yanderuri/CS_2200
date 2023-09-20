e1b07a482a6202b94d471360ba85dc6240435ce8
Mai, Vy <hmai35@gatech.edu>
2023-09-20 00:42:23 -0400

    fixed up project1 blt and bgt confusion. added enw bits for project2

--- projects/cs2200-project-1/assembly/test.s
+++ projects/cs2200-project-1/assembly/test.s
@@ -1,3 +1,7 @@
-main: beq $zero, $zero, continue
-add $a0, $a0, $zero
-continue: beq $zero, $zero, main
+addi $a0, $zero, 0x05
+addi $a1, $zero, 0x03
+blt $a0, $a1, continue
+main: blt $a1, $a0, continue
+add $a2, $zero, $zero
+continue: bgt $a1, $a0, main
+halt
